5c65f33ee438d55cdb8750b1b4f891025e33d0b4
kiranbj310 <kirankumar.xworkz@gmail.com>
2022-09-22 13:06:03 +0530

    adding java file to arrays

---
+++ Degree.java
@@ -0,0 +1,11 @@
+class Degree{
+   public static void main (String kiran[]){
+   
+   String typesofDegree[]={"BA", "Bcom", "BCA", "MBBS","BE","B.tech","Bsc"};
+   
+   System.out.println("The Types of  Degree");
+   System.out.println(typesofDegree[0]+"\n"+typesofDegree[1]+"\n"+typesofDegree[2]+"\n"+typesofDegree[3]+"\n"+typesofDegree[4]+"\n"+typesofDegree[5]+"\n"+typesofDegree[6]);
+   
+   }
+
+}
---
+++ Voot.java
@@ -0,0 +1,43 @@
+class Voot{
+    public static void main(String kiran []){
+	String kannadaSeries[]={"kannadathi","devaki Nandana","Olave Vismaya","chat corner","Nannarasi Radhe",
+	"Chakravarthy Ashoka","Ashwini Nakshathra","Radha Ramana","mangala Gavri Maduve","Lakshmi Baramma","Naga Kannike","Papa Pandu",
+	"Mooru Gantu","Kulavadhu","Prema Baraha"};
+	String hindiSeries[]={"Asur","Marzi","The Raikar Case","Illegal","The Gone Game",
+     "Crackdown","Sumer Singh Case File","Khwabone Ke Parindey","Time Out","Apaharan2","Code M","London Files","Candy",	
+     "shaadi Boys", "Criminal Justice Behind Closed Doors"};
+	String englishSeries[]={"The Plot Against America" ,"City On A Hill","Billions","The Outsider",
+	"Chernobyl","The White Lotus","The Mandalorian","Black Monday","Watchmen","Big Little Lies",
+	"True Detective","Succession","Jett","Modern Family","Sharp Objects"};
+	
+	  System.out.println("The list of kannadaSeries available in Voot:- ");
+	  
+	System.out.println(kannadaSeries[0]+" "+kannadaSeries[1]+" "+kannadaSeries[2]+" "+kannadaSeries[3]+" "+
+	kannadaSeries[4]+" "+kannadaSeries[5]+" "+kannadaSeries[6]+" "+kannadaSeries[7]+" "+kannadaSeries[8]+" "+
+	kannadaSeries[9]+" "+kannadaSeries[10]+" "+	kannadaSeries[11]+" "+kannadaSeries[12]+" "+kannadaSeries[13]
+	+" "+kannadaSeries[14]);
+	
+		System.out.println("****************************");
+		System.out.println("The list of hindiSeries available in Voot:- ");
+		
+	System.out.println(hindiSeries[0]+" "+hindiSeries[1]+" "+hindiSeries[2]+" "+hindiSeries[3]+" "+
+	hindiSeries[4]+" "+hindiSeries[5]+" "+hindiSeries[6]+" "+hindiSeries[7]+" "+hindiSeries[8]+" "+
+	hindiSeries[9]+" "+hindiSeries[10]+" "+	hindiSeries[11]+" "+hindiSeries[12]+" "+hindiSeries[13]
+	+" "+hindiSeries[14]);
+	
+		System.out.println("***************************");
+		System.out.println("The list of englishSeries available in Voot:- ");
+		
+	System.out.println(englishSeries[0]+" "+englishSeries[1]+" "+englishSeries[2]+" "+englishSeries[3]+" "+
+	englishSeries[4]+" "+englishSeries[5]+" "+englishSeries[6]+" "+englishSeries[7]+" "+englishSeries[8]+" "+
+	englishSeries[9]+" "+englishSeries[10]+" "+	englishSeries[11]+" "+englishSeries[12]+" "+englishSeries[13]
+	+" "+englishSeries[14]);
+	
+	 
+	}
+
+
+
+
+
+}
