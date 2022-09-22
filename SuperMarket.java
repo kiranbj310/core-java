e6ca15c067feb6ad3d86b4417290749f6417e774
kiranbj310 <kirankumar.xworkz@gmail.com>
2022-09-22 12:45:55 +0530

    adding java file to arrays

---
+++ SuperMarket.java
@@ -0,0 +1,62 @@
+class SuperMarket{
+
+
+  public static void main(String a[]){
+     
+      String vegatables[]= {"Ladys Finger","tomato","Heere kai","Cauliflower"};
+	  String perfumes[]= {"foge","wild stone","Ks","set wet"};
+	  String fruits[]= {"banana","apple","grape","orange","watermelon","pineapple","jackfruit","mango","lemon","papaya"};
+	  String biscuits[]= {"parle g","nimkin","krackjack","monaco","happyhappy","tiger","good day","bourbon","little hearts","marie gold"};
+	  String chocolates[]= {"dairy milk","perk","fuse","5 star","bournville","dairy milk silk","milkybar","kitkat","munch","snickers"};
+	  String groceries[]={"Soda","Cereal","Frozen Dinners","Salty Snacks","Milk","Laundry Detergent","Eggs",
+      "Peanut Butter and Jelly","Packaged Meats","Bread"};
+      String meat[]={"Chicken","Goat","Lamb","Beef","Pig's ","Fish","Crab","Lobster","Mutton","Duck"};
+      String shoes[]={"Athletic shoes","Ballet flats","Boat shoes", "Brogue shoes", "Clogs","Espadrilles", "Flip flops",
+       "High heels", "Loafers","Oxford shoes"};
+      String cosmetics[]={"skin moisturizers", "perfumes", "lipsticks", "powder","fingernail polishes","shampoos",
+      "permanent waves","hair colors","toothpastes","deodorants"};
+      String electronicsDevices[]={"smartphones","computers","laptops", "radios", "television receivers ", "video cameras", 
+      "digital audio players", "music systems", "microwave appliances","global position systems"};
+   
+	   System.out.println("List of  vegatables");
+	   System.out.println(vegatables[0]+ "\n"+vegatables[1]+ "\n"+ vegatables[2]+ "\n"+ vegatables[3]);
+	       
+       System.out.println("List of perfumes");
+	   System.out.println(perfumes[0]+ "\n"+perfumes[1]+ "\n"+perfumes[2]+ "\n"+perfumes[3]);
+       
+	   System.out.println("List of fruits");
+	   System.out.println(fruits[0]+ "\n"+fruits[1]+ "\n"+fruits[2]+ "\n"+fruits[3]+ "\n"+fruits[4]+ "\n"+fruits[5]+ "\n"+fruits[6]
+	   + "\n"+fruits[7]+ "\n"+fruits[8]+ "\n"+fruits[9]);
+	   
+	   System.out.println("List of biscuits");
+	   System.out.println(biscuits[0]+ "\n"+biscuits[1]+ "\n"+biscuits[2]+ "\n"+biscuits[3]+ "\n"+biscuits[4]+ "\n"+biscuits[5]+ "\n"+biscuits[6]
+	   + "\n"+biscuits[7]+ "\n"+biscuits[8]+ "\n"+biscuits[9]);
+
+	   System.out.println("List of chocolates");
+	   System.out.println(chocolates[0]+ "\n"+chocolates[1]+ "\n"+chocolates[2]+ "\n"+chocolates[3]+ "\n"+chocolates[4]+ "\n"+chocolates[5]+ "\n"+chocolates[6]
+	   + "\n"+chocolates[7]+ "\n"+chocolates[8]+ "\n"+chocolates[9]);
+	  
+	   System.out.println("list of the groceries:-");
+       System.out.println(groceries[0]+"\n"+ groceries[1]+"\n"+ groceries[2]+"\n"+ groceries[3]+"\n"+ groceries[4]
+       +"\n"+ groceries[5]+"\n"+ groceries[6]+"\n"+groceries[7]+"\n"+ groceries[8]+"\n"+groceries[9]); 
+	   
+       System.out.println("list of the meat:-");
+       System.out.println(meat[0]+"\n"+ meat[1]+"\n"+ meat[2]+"\n"+ meat[3]+"\n"+ meat[4]+"\n"+ meat[5]+"\n"+ meat[6]
+       +"\n"+meat[7]+"\n"+ meat[8]+"\n"+meat[9]); 
+	   
+       System.out.println("list of the shoes:-");
+       System.out.println(shoes[0]+"\n"+ shoes[1]+"\n"+ shoes[2]+"\n"+ shoes[3]+"\n"+ shoes[4]+"\n"+ shoes[5]+"\n"+ shoes[6]
+       +"\n"+shoes[7]+"\n"+ shoes[8]+"\n"+shoes[9]); 
+	   
+       System.out.println("list of the cosmetics:-");
+       System.out.println(cosmetics[0]+"\n"+ cosmetics[1]+"\n"+ cosmetics[2]+"\n"+ cosmetics[3]+"\n"+ cosmetics[4]
+       +"\n"+ cosmetics[5]+"\n"+ cosmetics[6]+"\n"+cosmetics[7]+"\n"+ cosmetics[8]+"\n"+cosmetics[9]); 
+	   
+       System.out.println("list of the electronicsDevices:-");
+       System.out.println(electronicsDevices[0]+"\n"+ electronicsDevices[1]+"\n"+ electronicsDevices[2]+"\n"+ electronicsDevices[3]
+       +"\n"+ electronicsDevices[4]+"\n"+ electronicsDevices[5]+"\n"+ electronicsDevices[6]+"\n"+electronicsDevices[7]
+       +"\n"+ electronicsDevices[8]+"\n"+electronicsDevices[9]); 
+  
+	   
+		   }
+	}		  
